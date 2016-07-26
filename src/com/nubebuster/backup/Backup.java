9ef5f732269198f29818b03227dc5c9fe26f2f9b
NubeBuster <kiwiszijncool@gmail.com>
2016-07-26 16:31:36 +0200

    Initial Commit

---
+++ src/com/nubebuster/backup/Backup.java
@@ -0,0 +1,185 @@
+package com.nubebuster.backup;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class Backup extends JavaPlugin {
+
+	private static Backup inst;
+	private static FileConfiguration config;
+
+	private long lastBackup, backupdelay;
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public void onEnable() {
+		inst = this;
+		configs();
+		if (config.isSet("lastbackup"))
+			lastBackup = config.getLong("lastbackup");
+		if (config.isSet("delay"))
+			backupdelay = config.getLong("delay");
+		else {
+			backupdelay = 86400000;
+			System.out.println("'delay' is not set in the config, setting to 24 hours...");
+		}
+
+		if (backupdelay != -1) {
+			if (lastBackup - System.currentTimeMillis() > backupdelay)
+				Bukkit.getScheduler().scheduleAsyncDelayedTask(this, new Runnable() {
+					public void run() {
+						try {
+							backup();
+							System.out.println("Finished backup.");
+						} catch (Exception e) {
+							e.printStackTrace();
+							System.out.println("An error ocurred during the backup!");
+						}
+					}
+				});
+			long next = (backupdelay - (System.currentTimeMillis() - lastBackup)) / 50;
+			Bukkit.getScheduler().scheduleAsyncRepeatingTask(this, new Runnable() {
+				public void run() {
+					try {
+						backup();
+						System.out.println("Finished backup.");
+					} catch (Exception e) {
+						e.printStackTrace();
+						System.out.println("An error ocurred during the backup!");
+					}
+				}
+			}, next > 0 ? next : 40, backupdelay / 50);
+		}
+		commands();
+	}
+
+	private void backup() throws Exception {
+		lastBackup = System.currentTimeMillis();
+		config.set("lastbackup", lastBackup);
+		saveConfig();
+		System.out.println("Saving worlds before backup...");
+		for (World w : Bukkit.getWorlds())
+			w.save();
+		System.out.println("Running backup...");
+		String date = new SimpleDateFormat("yyyy-MM-dd HH;mm;ss").format(new Date());
+		File backupFolder = new File("backup");
+		if (!backupFolder.exists())
+			backupFolder.mkdir();
+		FileOutputStream fos = new FileOutputStream(new File(backupFolder, "backup " + date + ".zip"));
+		ZipOutputStream zos = new ZipOutputStream(fos);
+
+		List<File> files = new ArrayList<File>();
+
+		// TODO add files
+		for (String s : config.getStringList("backuplist")) {
+			File f = new File(s);
+			if (!f.exists())
+				continue;
+			if (f.isDirectory()) {
+				getFilesFromDirectory(f, files);
+			} else
+				files.add(f);
+		}
+
+		for (File f : files) {
+			if (f.length() == 0)
+				continue;
+			FileInputStream fis = new FileInputStream(f);
+			ZipEntry zipEntry = new ZipEntry(f.getPath());
+			zos.putNextEntry(zipEntry);
+
+			byte[] bytes = new byte[(int) f.length()];
+			int length;
+			while ((length = fis.read(bytes)) >= 0) {
+				zos.write(bytes, 0, length);
+			}
+
+			zos.closeEntry();
+			fis.close();
+		}
+		zos.close();
+		fos.close();
+		if (config.isSet("deleteifolderthan") && config.getLong("deleteifolderthan") > 0) {
+			System.out.println("Removing old backups");
+			for (File f : backupFolder.listFiles())
+				if (System.currentTimeMillis() - f.lastModified() > config.getLong("deleteifolderthan"))
+					f.delete();
+		}
+	}
+
+	private List<File> getFilesFromDirectory(File directory, List<File> to) {
+		for (File f : directory.listFiles()) {
+			if (f.isDirectory())
+				getFilesFromDirectory(f, to);
+			else
+				to.add(f);
+		}
+		return to;
+	}
+
+	private void commands() {
+		getCommand("backup").setExecutor(new CommandExecutor() {
+			@SuppressWarnings("deprecation")
+			@Override
+			public boolean onCommand(CommandSender sender, Command arg1, String arg2, String[] arg3) {
+				if (!sender.hasPermission("nubesbackup.backup")) {
+					sender.sendMessage(ChatColor.RED + "You don't have permission to perform this command!");
+					return false;
+				}
+				sender.sendMessage(ChatColor.GREEN + "Starting backup...");
+				Bukkit.getScheduler().scheduleAsyncDelayedTask(inst, new Runnable() {
+					public void run() {
+						try {
+							backup();
+							sender.sendMessage(ChatColor.GREEN + "Finished backup");
+						} catch (Exception e) {
+							e.printStackTrace();
+							sender.sendMessage(ChatColor.RED + "An error ocurred during the backup!");
+						}
+					}
+				});
+				return false;
+			}
+		});
+
+		getCommand("backupreload").setExecutor(new CommandExecutor() {
+			@Override
+			public boolean onCommand(CommandSender sender, Command arg1, String arg2, String[] arg3) {
+				if (!sender.hasPermission("nubesbackup.reload")) {
+					sender.sendMessage(ChatColor.RED + "You don't have permission to perform this command!");
+					return false;
+				}
+				reloadConfig();
+				sender.sendMessage(ChatColor.GREEN + "Config reloaded!");
+				return false;
+			}
+		});
+	}
+
+	private void configs() {
+		config = getConfig();
+		saveDefaultConfig();
+		if (config.getBoolean("rewriteconfig")) {
+			File file = new File(getDataFolder() + File.separator + "config.yml");
+			file.delete();
+			saveDefaultConfig();
+		}
+	}
+}
